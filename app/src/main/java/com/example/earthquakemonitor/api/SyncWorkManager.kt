9398bc9d28eaea80703353ca0b859e33502153ff
Matias Ariel Barthelemi <mbarthelemi@payway.com.ar>
2023-12-10 09:04:41 -0300

    implementacion primera parte

---
+++ app/src/main/java/com/example/earthquakemonitor/api/SyncWorkManager.kt
@@ -0,0 +1,22 @@
+package com.example.earthquakemonitor.api
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.earthquakemonitor.database.getDatabase
+import com.example.earthquakemonitor.main.MainRepository
+
+class SyncWorkManager(appContext : Context, params : WorkerParameters): CoroutineWorker(appContext, params) {
+    companion object {
+        const val WORK_NAME: String =  "SyncWorkManager"
+    }
+
+    private val database = getDatabase(appContext)
+    private val repository = MainRepository(database)
+
+    override suspend fun doWork(): Result {
+        repository.fetchEarthQuakes(true)
+
+        return Result.success()
+    }
+}
